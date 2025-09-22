9fe8f64d94d37959b41e3a94cd5eb4545b84d78d
James Lee <aardvarkpepper@gmail.com>
2025-09-22 12:38:28 -0400

    Fix && references in Dashboard.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -14,12 +14,12 @@ const Dashboard = ({ user, setUser, setJwt }) => {
   return (
     <div className='dashboard'>
       <div>
-        {(user && user.role) === "" ? 'Please log in to enable functionality.  You may have been logged out if your session token expired.' : null}
-        {(user && user.role) === "sub" ? `Currently logged in as sub, username: ${user && user.username ? user.username : ""}.` : null}
-        {(user && user.role) === "admin" ? `Currently logged in as admin, username: ${user && user.username ? user.username : ""}.` : null}
+        {(user && user.role === "") ? 'Please log in to enable functionality.  You may have been logged out if your session token expired.' : null}
+        {(user && user.role === "sub") ? `Currently logged in as sub, username: ${(user && user.username) ? user.username : ""}.` : null}
+        {(user && user.role === "admin") ? `Currently logged in as admin, username: ${(user && user.username) ? user.username : ""}.` : null}
       </div>
       <div>
-        {user.username ? <button onClick={handleLogOut}>Log Out</button> : null}
+        {(user && user.username) ? <button onClick={handleLogOut}>Log Out</button> : null}
       </div>
     </div>
   )
